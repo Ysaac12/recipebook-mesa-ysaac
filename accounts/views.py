ee0b3a8fb615fdf49373fbf1728071692a5cc755
ysaac12 <ysaac.mesa@student.ateneo.edu>
2024-03-15 13:06:58 +0800

    added views for accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class CustomView(LoginRequiredMixin, TemplateView):
+    template_name = "registration/login.html"
+    redirect_field_name="accounts/login"

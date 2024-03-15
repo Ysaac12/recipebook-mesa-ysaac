075911ff18bd6cd8d0c8bd8aa84d5e609c73c03d
ysaac12 <ysaac.mesa@student.ateneo.edu>
2024-03-15 14:31:49 +0800

    models migrations for modified recipe

--- ledger/models.py
+++ ledger/models.py
@@ -3,6 +3,10 @@ from django.urls import reverse
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
     
